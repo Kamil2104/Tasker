38d761523ea39e8f790a4d0e8d4eb8bb3a794533
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 17:48:10 +0100

    finish GUI for login

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -5,11 +5,15 @@ const LoginPage = () => {
   return (
     <div className='loginPage'>
       <div className='loginContainer'>
-          <input type="text" name="login" id="login" placeholder="Login: " /> <br />
-          <input type="password" name="password" id="password" placeholder="Password: " />
-          <button id="btnLogin"> Log in </button>
-          <p> Forgot password? </p>
+        <div className='loginContainerContent'>
+          <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
+          <input type="password" name="password" id="password" placeholder="Password: " /> <br />
+          <button id="btnLogin"> Log in </button> <br />
+          <label> Forgot password? </label> <br />
+        </div>
+        <div className='loginContainerButtonCreateNewAccount'>
           <button id="btnCreateNewAccount"> Create new account </button>
+        </div>
       </div>
     </div>
   )
