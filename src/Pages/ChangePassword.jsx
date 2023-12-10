a58777a0e98d59c908ab86767f2e76188682ff46
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 19:46:52 +0100

    add components to ChangePassword.jsx

--- src/Pages/ChangePassword.jsx
+++ src/Pages/ChangePassword.jsx
@@ -3,7 +3,16 @@ import '../Styles/ChangePasswordStyle.css'
 
 export const ChangePassword = () => {
   return (
-    <div>ChangePassword</div>
+    <div className='changePasswordPage'>
+      <div className='changePasswordContainer'>
+        <div className='changePasswordContainerContent'>
+          <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
+          <input type="password" name="password1" id="password1" placeholder="Password: " /> <br />
+          <input type="password" name="password2" id="password2" placeholder="Password: " /> <br />
+          <button id="btnChangePassword"> Change password </button> <br />
+        </div>
+      </div>
+    </div>
   )
 }
 
