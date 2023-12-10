9cd7aa1e91dd2d717d2e0500e9e3f57d593e1c65
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 14:12:19 +0100

    add all needed components to LoginPage.jsx

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -6,15 +6,28 @@ const LoginPage = () => {
     <div className='loginPage'>
       <div className='titleSide'>
         <div className='upperTitleSide'>
-
+          <div className='upperTitleSideContent'>
+            <h1> Tasker </h1> <br />
+            <p> Create tasks so you will never forget what you have to do! </p>
+          </div>
         </div>
         <div className='lowerTitleSide'>
-
+          {/* Empty container */}
         </div>
       </div>
       <div className='loginSide'>
         <div className='loginContainer'>
-
+          <div className='loginContainerInputs'>
+            <input type="text" name="login" id="login" placeholder="Login: " /> <br />
+            <input type="password" name="password" id="password" placeholder="Password: " />
+          </div>
+          <div className='loginContainerEventComponents'>
+            <button id="btnLogin"> Log in </button>
+            <p> Forgot password? </p>
+              <div className='loginContainerEventComponentsButtonCreateNewAccount'>
+                <button id="btnCreateNewAccount"> Create new account </button>
+              </div>
+          </div>
         </div>
       </div>
     </div>
