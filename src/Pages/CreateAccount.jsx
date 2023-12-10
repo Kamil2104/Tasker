09b03c41a11afe504c0d59da4d57865d57ad1da7
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 20:09:31 +0100

    add components to CreateAccount.jsx

--- src/Pages/CreateAccount.jsx
+++ src/Pages/CreateAccount.jsx
@@ -9,7 +9,17 @@ export const CreateAccount = () => {
   }, []);
 
   return (
-    <div>CreateAccount</div>
+    <div className='createAccountPage'>
+      <div className='createAccountContainer'>
+        <div className='createAccountContainerContent'>
+          <h1> Create account </h1>
+          <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
+          <input type="password" name="password1" id="password1" placeholder="Password: " /> <br />
+          <input type="password" name="password2" id="password2" placeholder="Password: " /> <br />
+          <button id="btnCreateAccount"> Create account </button> <br />
+        </div>
+      </div>
+    </div>
   )
 }
 
