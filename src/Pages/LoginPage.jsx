6ca3e70a69597e6c3dd2d426ea428a6ab24b3dc5
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 15:24:18 +0100

    delete div's beacuse they were useless

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -5,15 +5,11 @@ const LoginPage = () => {
   return (
     <div className='loginPage'>
       <div className='loginContainer'>
-        <div className='loginContainerInputs'>
           <input type="text" name="login" id="login" placeholder="Login: " /> <br />
           <input type="password" name="password" id="password" placeholder="Password: " />
-        </div>
-        <div className='loginContainerEventComponents'>
           <button id="btnLogin"> Log in </button>
           <p> Forgot password? </p>
           <button id="btnCreateNewAccount"> Create new account </button>
-        </div>
       </div>
     </div>
   )
