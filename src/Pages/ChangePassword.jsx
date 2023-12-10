fe035b3e532795e20955f036cfaef815457a472f
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 20:05:19 +0100

    add h1 component in ChangePassword.jsx and do for it appearance modification

--- src/Pages/ChangePassword.jsx
+++ src/Pages/ChangePassword.jsx
@@ -12,6 +12,7 @@ const ChangePassword = () => {
     <div className='changePasswordPage'>
       <div className='changePasswordContainer'>
         <div className='changePasswordContainerContent'>
+          <h1> Change password </h1>
           <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
           <input type="password" name="password1" id="password1" placeholder="Password: " /> <br />
           <input type="password" name="password2" id="password2" placeholder="Password: " /> <br />
