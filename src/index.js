2e17ebf59f391a66a460be1390d5339d494749b0
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 19:49:03 +0100

    change showing file to be able to check if GUI for ChangePassword.jsx is properly working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginPage from './/Pages/LoginPage'
+import ChangePassword from './/Pages/ChangePassword'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <LoginPage />
+    <ChangePassword />
 );
 
 reportWebVitals();
