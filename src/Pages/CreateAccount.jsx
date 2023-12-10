f6f2124bd8f2031f592c3d3922c9a928285e3007
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 20:06:50 +0100

    change title for CreateAccount.jsx

--- src/Pages/CreateAccount.jsx
+++ src/Pages/CreateAccount.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useEffect } from 'react';
+
 import '../Styles/CreateAccountStyle.css'
 
 export const CreateAccount = () => {
+  useEffect(() => {
+    document.title = "Tasker - create account";
+  }, []);
+
   return (
     <div>CreateAccount</div>
   )
