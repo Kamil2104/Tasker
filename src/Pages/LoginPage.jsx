7fed3db3df17b4caf97da72e05422a4046b34027
pindrive <kamilniewiadomski18@gmail.com>
2023-12-10 15:11:51 +0100

    add some CSS and one div for better structure of website

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,14 +4,16 @@ import '../Styles/LoginPageStyle.css'
 const LoginPage = () => {
   return (
     <div className='loginPage'>
-      <div className='loginContainerInputs'>
-        <input type="text" name="login" id="login" placeholder="Login: " /> <br />
-        <input type="password" name="password" id="password" placeholder="Password: " />
-      </div>
-      <div className='loginContainerEventComponents'>
-        <button id="btnLogin"> Log in </button>
-        <p> Forgot password? </p>
-        <button id="btnCreateNewAccount"> Create new account </button>
+      <div className='loginContainer'>
+        <div className='loginContainerInputs'>
+          <input type="text" name="login" id="login" placeholder="Login: " /> <br />
+          <input type="password" name="password" id="password" placeholder="Password: " />
+        </div>
+        <div className='loginContainerEventComponents'>
+          <button id="btnLogin"> Log in </button>
+          <p> Forgot password? </p>
+          <button id="btnCreateNewAccount"> Create new account </button>
+        </div>
       </div>
     </div>
   )
