d05e0050a4c2abd7fc8984fdff554ee0a977659c
pindrive <kamilniewiadomski18@gmail.com>
2023-12-11 17:19:26 +0100

    make components for addTaskPanel in TasksPage.jsx

--- src/Pages/TasksPage.jsx
+++ src/Pages/TasksPage.jsx
@@ -5,7 +5,17 @@ const TasksPage = () => {
   return (
     <div className='tasksPage'>
       <div className='addTaskPanel'>
-
+        <h1> Create task: </h1>
+        <input type="text" placeholder="Name: " autoComplete="off" /> <br />
+        <input type="text" placeholder="Description: " autoComplete="off" /> <br />
+        <input type="date" autoComplete="off" /> <br />
+        <select name="selectPriority" id="selectPriority">
+          <option value="low"> low </option>
+          <option value="medium"> medium </option>
+          <option value="high"> high </option>
+        </select> <br />
+        <button id="btnAddTask"> Add </button> <br />
+        <button id="btnClearForm"> Clear </button> 
       </div>
       <div className='showTasksPanel'>
 
