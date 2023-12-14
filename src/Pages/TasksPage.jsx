a98978ad8ba0d1b508eb7a5eda25b867ef374e2c
pindrive <kamilniewiadomski18@gmail.com>
2023-12-14 18:06:08 +0100

    add CSS for textarea (in which will be displayed data from database for user's task) and textarea's in TaskPage.jsx to see how it's looking

--- src/Pages/TasksPage.jsx
+++ src/Pages/TasksPage.jsx
@@ -4,6 +4,11 @@ import { useState } from 'react';
 import '../Styles/TasksPageStyle.css'
 
 const TasksPage = () => {
+  const name = "Ogarnąć mieszkanieaaaaaaaaaaaaaa";
+  const description = "Posprzątać całe mieszkanie i zetrzeć kurze";
+  const date = "14.12.2023";
+  const priority = "High";
+
   const [selectedDate, setSelectedDate] = useState(getCurrentDate());
 
   function getCurrentDate() {
@@ -11,7 +16,7 @@ const TasksPage = () => {
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const day = String(now.getDate()).padStart(2, '0');
-    
+
     return `${year}-${month}-${day}`;
   }
 
@@ -32,26 +37,37 @@ const TasksPage = () => {
           <option value="medium"> medium </option>
           <option value="high"> high </option>
         </select> <br />
-        <button id="btnAddTask"> Add </button> 
-        <button id="btnClearForm"> Clear </button> 
+        <button id="btnAddTask"> Add </button>
+        <button id="btnClearForm"> Clear </button>
 
       </div>
       <div className='showTasksPanel'>
 
+          <textarea spellCheck="false" value={`ADKLJKALJDS: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
+          <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
       </div>
+      
       <div className='findTasksPanel'>
 
         <h1> Find tasks: </h1>
         <input type="text" placeholder="Name: " autoComplete="off" /> <br />
-        <input 
-        type="date" autoComplete="off" value={selectedDate} onChange={handleDateChange} min={getCurrentDate()} /> <br />
-          <select name="selectPriority" id="selectPriority">
-            <option value="low"> low </option>
-            <option value="medium"> medium </option>
-            <option value="high"> high </option>
-          </select> <br />
-          <button id="btnFindTasks"> Add </button>
-          <button id="btnClearForm"> Clear </button>  
+        <input
+          type="date" autoComplete="off" value={selectedDate} onChange={handleDateChange} min={getCurrentDate()} /> <br />
+        <select name="selectPriority" id="selectPriority">
+          <option value="low"> low </option>
+          <option value="medium"> medium </option>
+          <option value="high"> high </option>
+        </select> <br />
+        <button id="btnFindTasks"> Add </button>
+        <button id="btnClearForm"> Clear </button>
 
       </div>
     </div>
