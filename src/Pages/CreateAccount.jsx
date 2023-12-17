84ceb7aafaab17f1a5ddc69f3e97cb23d8e932e3
pindrive <kamilniewiadomski18@gmail.com>
2023-12-17 11:04:55 +0100

    format the code (add comments and break code lines in some places that the code will be easier to read) - in CreateAccount.jsx

--- src/Pages/CreateAccount.jsx
+++ src/Pages/CreateAccount.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import '../Styles/CreateAccountStyle.css'
 
 export const CreateAccount = () => {
+  // useEffect's
   useEffect(() => {
     document.title = "Tasker - create account";
   }, []);
@@ -13,9 +14,25 @@ export const CreateAccount = () => {
       <div className='createAccountContainer'>
         <div className='createAccountContainerContent'>
           <h1> Create account </h1>
-          <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
-          <input type="password" name="password1" id="password1" placeholder="Password: " /> <br />
-          <input type="password" name="password2" id="password2" placeholder="Password: " /> <br />
+          <input 
+            type="text" 
+            name="login" 
+            id="login" 
+            placeholder="Login: " 
+          autoComplete="off" 
+          /> <br />
+          <input 
+            type="password" 
+            name="password1" 
+            id="password1" 
+            placeholder="Password: " 
+          /> <br />
+          <input 
+            type="password" 
+            name="password2" 
+            id="password2" 
+            placeholder="Password: " 
+          /> <br />
           <button id="btnCreateAccount"> Create account </button> <br />
         </div>
       </div>
