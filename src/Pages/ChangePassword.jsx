4eb954d6727c3887d298a9422eceea66ca3dc78f
pindrive <kamilniewiadomski18@gmail.com>
2023-12-17 11:45:23 +0100

    add functionality for visibility of password (and add CSS for it) - will have to make changes beacuse of resposibility (in ChangePassword.jsx)

--- src/Pages/ChangePassword.jsx
+++ src/Pages/ChangePassword.jsx
@@ -1,38 +1,64 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 import '../Styles/ChangePasswordStyle.css'
 
 const ChangePassword = () => {
+  // useState's
+  const [showPassword1, setShowPassword1] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
   // useEffect's
   useEffect(() => {
     document.title = "Tasker - change password";
   }, []);
+
+  // functions
+  const togglePassword1Visibility = () => {
+    setShowPassword1((prev) => !prev);
+  };
+
+  const togglePassword2Visibility = () => {
+    setShowPassword2((prev) => !prev);
+  };
 
   return (
     <div className='changePasswordPage'>
       <div className='changePasswordContainer'>
         <div className='changePasswordContainerContent'>
           <h1> Change password </h1>
-          <input 
-            type="text" 
-            name="login" 
-            id="login" 
-            placeholder="Login: " 
-            autoComplete="off" 
+          <input
+            type="text"
+            name="login"
+            id="login"
+            placeholder="Login: "
+            autoComplete="off"
           /> <br />
-          <input 
-            type="password" 
-            name="password1" 
-            id="password1" 
-            placeholder="Password: " 
-          /> <br />
-          <input 
-            type="password" 
-            name="password2" 
-            id="password2" 
-            placeholder="Password: " 
-          /> <br />
+          <input
+            type={showPassword1 ? 'text' : 'password'}
+            name="password1"
+            id="password1"
+            placeholder="Password: "
+          />
+          <FontAwesomeIcon
+            icon={showPassword1 ? faEyeSlash : faEye}
+            onClick={togglePassword1Visibility}
+            className="passwordVisibilityIcon"
+          /><br />
+          <input
+            type={showPassword2 ? 'text' : 'password'}
+            name="password2"
+            id="password2"
+            placeholder="Password:"
+          />
+          <FontAwesomeIcon
+            icon={showPassword2 ? faEyeSlash : faEye}
+            onClick={togglePassword2Visibility}
+            className="passwordVisibilityIcon"
+          /><br />
           <button id="btnChangePassword"> Change password </button> <br />
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TasksPage from './/Pages/TasksPage'
+import ChangePassword from './/Pages/ChangePassword'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <TasksPage />
+    <ChangePassword />
 );
 
 reportWebVitals();
