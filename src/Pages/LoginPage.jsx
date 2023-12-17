a354fcb8d1b906b87bd2e73d2741efbfbb320996
pindrive <kamilniewiadomski18@gmail.com>
2023-12-17 11:03:41 +0100

    format the code (add comments and break code lines in some places that the code will be easier to read) - in LoginPage.jsx

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import '../Styles/LoginPageStyle.css';  
 
 const LoginPage = () => {
+  // useEffect's
   useEffect(() => {
     document.title = "Tasker - log in";
   }, []);
@@ -13,8 +14,19 @@ const LoginPage = () => {
       <div className='loginContainer'>
         <div className='loginContainerContent'>
           <h1> Log in </h1>
-          <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
-          <input type="password" name="password" id="password" placeholder="Password: " /> <br />
+          <input 
+            type="text" 
+            name="login" 
+            id="login" 
+            placeholder="Login: " 
+            autoComplete="off" 
+          /> <br />
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            placeholder="Password: " 
+          /> <br />
           <button id="btnLogin"> Log in </button> <br />
           <label> Forgot password? </label> <br />
         </div>
