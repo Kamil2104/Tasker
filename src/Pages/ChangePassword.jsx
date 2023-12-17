9b871412aba3650de088091b270d70d95688f3bc
pindrive <kamilniewiadomski18@gmail.com>
2023-12-17 11:06:03 +0100

    format the code (add comments and break code lines in some places that the code will be easier to read) - in ChangePassword.jsx

--- src/Pages/ChangePassword.jsx
+++ src/Pages/ChangePassword.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import '../Styles/ChangePasswordStyle.css'
 
 const ChangePassword = () => {
+  // useEffect's
   useEffect(() => {
     document.title = "Tasker - change password";
   }, []);
@@ -13,9 +14,25 @@ const ChangePassword = () => {
       <div className='changePasswordContainer'>
         <div className='changePasswordContainerContent'>
           <h1> Change password </h1>
-          <input type="text" name="login" id="login" placeholder="Login: " autoComplete="off" /> <br />
-          <input type="password" name="password1" id="password1" placeholder="Password: " /> <br />
-          <input type="password" name="password2" id="password2" placeholder="Password: " /> <br />
+          <input 
+            type="text" 
+            name="login" 
+            id="login" 
+            placeholder="Login: " 
+            autoComplete="off" 
+          /> <br />
+          <input 
+            type="password" 
+            name="password1" 
+            id="password1" 
+            placeholder="Password: " 
+          /> <br />
+          <input 
+            type="password" 
+            name="password2" 
+            id="password2" 
+            placeholder="Password: " 
+          /> <br />
           <button id="btnChangePassword"> Change password </button> <br />
         </div>
       </div>
