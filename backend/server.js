704dca18ffc7ef2796591f24802aa69913dd0f48
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 17:25:19 +0100

    initializing the Express application

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+
+// Initializing the Express application
+const app = express();
+app.use(cors());
