a620722516cfb53ecc3f80950aa3b732dec2618f
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 17:26:27 +0100

    configuring the connection to the MySQL database

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,11 @@ const cors = require("cors");
 // Initializing the Express application
 const app = express();
 app.use(cors());
+
+// Configuring the connection to the MySQL database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "taskerdb"
+})
