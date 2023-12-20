202bad306b8cc7e9628997131faac774a03b1190
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 17:42:23 +0100

    add comment on top of the code

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+// Comments are only in backend, beacuse it's my first time with it and im trying to learn
+
 // Import modules
 const express = require("express");
 const mysql = require("mysql");
@@ -18,4 +20,8 @@ const db = mysql.createConnection({
 // Checking if server on port 8081 is running (if it is, then show information on console)
 app.listen(8081, () => {
     console.log("Listening");
+})
+
+app.post('/createAccount', (req, res) => {
+    const sql = "INSERT INTO login ('login', 'password') values (?)";
 })
