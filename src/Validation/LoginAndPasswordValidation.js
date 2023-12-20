d46745417e38ea85abfa5c2010e3e0eccf451c9f
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 20:37:43 +0100

    add function to validate login length

--- src/Validation/LoginAndPasswordValidation.js
+++ src/Validation/LoginAndPasswordValidation.js
@@ -1,10 +1,16 @@
-function loginValidation() {
+const errorMsg = ""
 
+// LOGIN VALIDATIONS
+
+function loginValidation(login) {
+    
 }
 
 function newLoginValidation() {
-    
+
 }
+
+// PASSWORD VALIDATIONS
 
 function singlePasswordValidation() {
 
@@ -12,4 +18,24 @@ function singlePasswordValidation() {
 
 function doublePasswordValidation() {
 
+}
+
+// OTHER VALIDATIONS
+function loginLengthValidator(login) {
+    if (login != "") {
+        if (login.length() >= 5) {
+            if (login.length() <= 20) {
+                return true
+            } else {
+                errorMsg = "Login is to long!"
+                return false
+            }
+        } else {
+            errorMsg = "Login is to short!"
+            return false
+        }
+    } else {
+        errorMsg = "Login can't be empty!"
+        return false
+    }
 }
