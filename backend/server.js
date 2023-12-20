aa4d9ffe31a28acf024d32f05accce4c2ffa060c
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 17:40:52 +0100

    add listener for application to check if server is running (show information on console)

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,8 @@ const db = mysql.createConnection({
     password: "",
     database: "taskerdb"
 })
+
+// Checking if server on port 8081 is running (if it is, then show information on console)
+app.listen(8081, () => {
+    console.log("Listening");
+})
