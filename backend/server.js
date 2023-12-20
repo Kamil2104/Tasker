016592657514babc90d196a8cd89b46d40f2a11e
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 17:47:20 +0100

    add POST endpoint for user account creation

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,12 @@ app.listen(8081, () => {
     console.log("Listening");
 })
 
+app.post('/createAccount', (req, res) => {
+
+})
+
+const sql = "INSERT INTO login ('login', 'password') values (?)";
+const values = [
+        
+]
+db.query(sql, []);
