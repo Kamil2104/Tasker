e0f9bad4ac1817384009d834981617045d410052
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 18:04:09 +0100

    add form in OrderTasksPanel.jsx

--- src/Pages/TasksPage pages/OrderTasksPanel.jsx
+++ src/Pages/TasksPage pages/OrderTasksPanel.jsx
@@ -4,22 +4,23 @@ const OrderTasksPanel = () => {
     return (
         <div className='orderTasksPanel'>
             <h1> Order by: </h1> <br />
-            <select
-                name="selectOrderBy"
-                id="selectOrderBy">
-                <option> Name </option>
-                <option> Description </option>
-                <option> Date </option>
-                <option> Priority </option>
-            </select>
-            <select
-                name="selectOrderType"
-                id="selectOrderType">
-                <option> ASC </option>
-                <option> DESC </option>
-            </select>
-            <button id="setOrder"> Set </button>
-
+            <form method="post" action='/orderTasks'>
+                <select
+                    name="selectOrderBy"
+                    id="selectOrderBy">
+                    <option> Name </option>
+                    <option> Description </option>
+                    <option> Date </option>
+                    <option> Priority </option>
+                </select>
+                <select
+                    name="selectOrderType"
+                    id="selectOrderType">
+                    <option> ASC </option>
+                    <option> DESC </option>
+                </select>
+                <button id="setOrder"> Set </button>
+            </form>
         </div>
     )
 }
