5f0a86c8f1fbfc1af3e8e514f2aad5bfa91d9472
pindrive <kamilniewiadomski18@gmail.com>
2023-12-20 17:13:44 +0100

    create folder for backend and install there necessary files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TasksPage from './/Pages/TasksPage'
 import reportWebVitals from './reportWebVitals';
+import LoginPage from './Pages/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <TasksPage />
+    <LoginPage />
 );
 
 reportWebVitals();
