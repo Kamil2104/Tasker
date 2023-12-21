faecd5f43b2d1b9a2dc1805fe217a0c9cb24da18
pindrive <kamilniewiadomski18@gmail.com>
2023-12-21 13:06:59 +0100

    format the code

--- src/Pages/ChangePassword.jsx
+++ src/Pages/ChangePassword.jsx
@@ -46,41 +46,41 @@ const ChangePassword = () => {
       <div className='changePasswordContainer'>
         <div className='changePasswordContainerContent'>
           <h1> Change password </h1>
-            <input
-              type="text"
-              name="login"
-              id="login"
-              placeholder="Login: "
-              autoComplete="off"
-              onChange={handleInputLogin}
-            /> <br />
-            <input
-              type={showPassword1 ? 'text' : 'password'}
-              name="password1"
-              id="password1"
-              placeholder="Password: "
-              autoComplete="off"
-              onChange={handleInputPassword1}
-            />
-            <FontAwesomeIcon
-              icon={showPassword1 ? faEyeSlash : faEye}
-              onClick={togglePassword1Visibility}
-              className="passwordVisibilityIcon"
-            /><br />
-            <input
-              type={showPassword2 ? 'text' : 'password'}
-              name="password2"
-              id="password2"
-              placeholder="Password:"
-              autoComplete="off"
-              onChange={handleInputPassword2}
-            />
-            <FontAwesomeIcon
-              icon={showPassword2 ? faEyeSlash : faEye}
-              onClick={togglePassword2Visibility}
-              className="passwordVisibilityIcon"
-            /><br />
-            <button id="btnChangePassword"> Change password </button> <br />
+          <input
+            type="text"
+            name="login"
+            id="login"
+            placeholder="Login: "
+            autoComplete="off"
+            onChange={handleInputLogin}
+          /> <br />
+          <input
+            type={showPassword1 ? 'text' : 'password'}
+            name="password1"
+            id="password1"
+            placeholder="Password: "
+            autoComplete="off"
+            onChange={handleInputPassword1}
+          />
+          <FontAwesomeIcon
+            icon={showPassword1 ? faEyeSlash : faEye}
+            onClick={togglePassword1Visibility}
+            className="passwordVisibilityIcon"
+          /><br />
+          <input
+            type={showPassword2 ? 'text' : 'password'}
+            name="password2"
+            id="password2"
+            placeholder="Password:"
+            autoComplete="off"
+            onChange={handleInputPassword2}
+          />
+          <FontAwesomeIcon
+            icon={showPassword2 ? faEyeSlash : faEye}
+            onClick={togglePassword2Visibility}
+            className="passwordVisibilityIcon"
+          /><br />
+          <button id="btnChangePassword"> Change password </button> <br />
         </div>
       </div>
     </div>
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -20,61 +20,61 @@ const LoginPage = () => {
     document.title = "Tasker - log in";
   }, []);
 
-    // functions
-    const handleInputLogin = (event) => {
-      setLoginText(event.target.value);
-    }
+  // functions
+  const handleInputLogin = (event) => {
+    setLoginText(event.target.value);
+  }
 
-    const handleInputPassword = (event) => {
-      setPasswordText(event.target.value);
-    }
+  const handleInputPassword = (event) => {
+    setPasswordText(event.target.value);
+  }
 
-    const handleButtonLogin = () => {
-      let loginResponse = validateLogin(loginText);
-      let passwordResponse = validatePassword(passwordText);
+  const handleButtonLogin = () => {
+    let loginResponse = validateLogin(loginText);
+    let passwordResponse = validatePassword(passwordText);
 
-      if (loginResponse === "") {
-        if (passwordResponse === "") {
-          // CODE USING DATABASE
-        } else {
-          alert (passwordResponse)
-        }
+    if (loginResponse === "") {
+      if (passwordResponse === "") {
+        // CODE USING DATABASE
       } else {
-        alert(loginResponse);
+        alert(passwordResponse)
       }
+    } else {
+      alert(loginResponse);
     }
+  }
 
-    const togglePasswordVisibility = () => {
-      setShowPassword((prev) => !prev);
-    };
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   return (
     <div className='loginPage'>
       <div className='loginContainer'>
         <div className='loginContainerContent'>
           <h1> Log in </h1>
-            <input
-              type="text"
-              name="login"
-              id="login"
-              placeholder="Login: "
-              autoComplete="off"
-              onChange={handleInputLogin}
-            /> <br />
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              id="password"
-              placeholder="Password: "
-              autoComplete="off"
-              onChange={handleInputPassword}
-            />
-            <FontAwesomeIcon
-              icon={showPassword ? faEyeSlash : faEye}
-              onClick={togglePasswordVisibility}
-              className="passwordVisibilityIcon"
-            /><br />
-            <button id="btnLogin" onClick={handleButtonLogin}> Log in </button> <br />
+          <input
+            type="text"
+            name="login"
+            id="login"
+            placeholder="Login: "
+            autoComplete="off"
+            onChange={handleInputLogin}
+          /> <br />
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            id="password"
+            placeholder="Password: "
+            autoComplete="off"
+            onChange={handleInputPassword}
+          />
+          <FontAwesomeIcon
+            icon={showPassword ? faEyeSlash : faEye}
+            onClick={togglePasswordVisibility}
+            className="passwordVisibilityIcon"
+          /><br />
+          <button id="btnLogin" onClick={handleButtonLogin}> Log in </button> <br />
           <label> Forgot password? </label> <br />
         </div>
         <div className='loginContainerButtonCreateNewAccount'>
