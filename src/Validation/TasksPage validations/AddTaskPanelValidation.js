30053283ed6d9285353605f792844d62be841b3e
pindrive <kamilniewiadomski18@gmail.com>
2023-12-21 14:09:07 +0100

    create functionality for nameLength function

--- src/Validation/TasksPage validations/AddTaskPanelValidation.js
+++ src/Validation/TasksPage validations/AddTaskPanelValidation.js
@@ -1,9 +1,15 @@
+let errorMsg = ""
+
 // ADD TASK PANEL VALIDATIONS
 
 function validateAddingTask(name, description) {
     if (!isEmpty(name)) {
         if (!isEmpty(description)) {
+            if (nameLength(name)) {
 
+            } else {
+                return errorMsg
+            }
         } else {
             return "Description can't be empty."
         }
@@ -15,7 +21,17 @@ function validateAddingTask(name, description) {
 // OTHER VALIDATIONS
 
 function nameLength(name) {
-
+    if (name.length >= 5) {
+        if (name.length <= 20) {
+            return true
+        } else {
+            errorMsg = "Name is too long."
+            return false
+        }
+    } else {
+        errorMsg = "Name is too short."
+        return false
+    }
 }
 
 function descriptionLength(description) {
