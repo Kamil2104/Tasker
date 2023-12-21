4bff678893ee35b2b224f43952cecbbe2983f421
pindrive <kamilniewiadomski18@gmail.com>
2023-12-21 21:40:14 +0100

    add functionality for FindTasksPanelValidation.js

--- src/Validation/FindTasksPanelValidation.js
+++ src/Validation/FindTasksPanelValidation.js
@@ -1,3 +1,39 @@
-function validateFindingTasks() {
+errorMsg = ""
 
+// FIND TASKS PANEL VALIDATIONS
+
+function validateFindingTasks(name) {
+    if (!isEmpty(name)) {
+        if (nameLength(name)) {
+            return ""
+        } else {
+            return errorMsg
+        }
+    } else {
+        return "Name can't be empty!"
+    }
+}
+
+// OTHER VALIDATIONS
+
+function nameLength(name) {
+    if (name.length >= 5) {
+        if (name.length <= 20) {
+            return true
+        } else {
+            errorMsg = "Name is too long."
+            return false
+        }
+    } else {
+        errorMsg = "Name is too short."
+        return false
+    }
+}
+
+function isEmpty(name) {
+    if (name === "") {
+        return true
+    } else {
+        return false
+    }
 }
