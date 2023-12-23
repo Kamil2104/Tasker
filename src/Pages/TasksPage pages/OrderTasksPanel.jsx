bd44d13cf5fb5fa7fce43d47ac6b59cf8986b3aa
pindrive <kamilniewiadomski18@gmail.com>
2023-12-23 11:11:25 +0100

    delete form in OrderTasksPanel.jsx

--- src/Pages/TasksPage pages/OrderTasksPanel.jsx
+++ src/Pages/TasksPage pages/OrderTasksPanel.jsx
@@ -4,23 +4,21 @@ const OrderTasksPanel = () => {
     return (
         <div className='orderTasksPanel'>
             <h1> Order by: </h1> <br />
-            <form method="post" action='/orderTasks'>
-                <select
-                    name="selectOrderBy"
-                    id="selectOrderBy">
-                    <option> Name </option>
-                    <option> Description </option>
-                    <option> Date </option>
-                    <option> Priority </option>
-                </select>
-                <select
-                    name="selectOrderType"
-                    id="selectOrderType">
-                    <option> ASC </option>
-                    <option> DESC </option>
-                </select>
-                <button id="setOrder"> Set </button>
-            </form>
+            <select
+                name="selectOrderBy"
+                id="selectOrderBy">
+                <option> Name </option>
+                <option> Description </option>
+                <option> Date </option>
+                <option> Priority </option>
+            </select>
+            <select
+                name="selectOrderType"
+                id="selectOrderType">
+                <option> ASC </option>
+                <option> DESC </option>
+            </select>
+            <button id="setOrder"> Set </button>
         </div>
     )
 }
