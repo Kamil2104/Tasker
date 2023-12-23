7d5cca6a64844e775cf1f7149047e8b09a311411
pindrive <kamilniewiadomski18@gmail.com>
2023-12-23 11:17:25 +0100

    add containers in OrderTasksPanel.jsx for better positioning

--- src/Pages/TasksPage pages/OrderTasksPanel.jsx
+++ src/Pages/TasksPage pages/OrderTasksPanel.jsx
@@ -3,22 +3,26 @@ import React from 'react'
 const OrderTasksPanel = () => {
     return (
         <div className='orderTasksPanel'>
-            <h1> Order by: </h1> <br />
-            <select
-                name="selectOrderBy"
-                id="selectOrderBy">
-                <option> Name </option>
-                <option> Description </option>
-                <option> Date </option>
-                <option> Priority </option>
-            </select>
-            <select
-                name="selectOrderType"
-                id="selectOrderType">
-                <option> ASC </option>
-                <option> DESC </option>
-            </select>
-            <button id="setOrder"> Set </button>
+            <div className='orderTasksPanelHeader'>
+                <h1> Order by: </h1> <br />
+            </div>
+            <div className='orderTasksPanelCriterias'>
+                <select
+                    name="selectOrderBy"
+                    id="selectOrderBy">
+                    <option> Name </option>
+                    <option> Description </option>
+                    <option> Date </option>
+                    <option> Priority </option>
+                </select>
+                <select
+                    name="selectOrderType"
+                    id="selectOrderType">
+                    <option> ASC </option>
+                    <option> DESC </option>
+                </select>
+                <button id="setOrder"> Set </button>
+            </div>
         </div>
     )
 }
