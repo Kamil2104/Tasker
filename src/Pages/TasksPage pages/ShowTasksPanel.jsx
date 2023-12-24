d78a7a84da572df591eb062e4d7a93506a7e21a2
pindrive <kamilniewiadomski18@gmail.com>
2023-12-24 12:48:17 +0100

    add working (in 90%) showTasksPanel.jsx using axios

--- src/Pages/TasksPage pages/ShowTasksPanel.jsx
+++ src/Pages/TasksPage pages/ShowTasksPanel.jsx
@@ -1,47 +1,49 @@
-import React, { useEffect } from 'react'
-
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ShowTasksPanel = ({ login }) => {
-    // const name = "Ogarnąć mieszkanieaaaaaaaaaaaaaa";
-    // const description = "Posprzątać całe mieszkanie i zetrzeć kurze";
-    // const date = "14.12.2023";
-    // const priority = "High";
+    const noTasksHeader = "No tasks available.";
+    const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
         const values = {
             user: login
-        }
+        };
 
         axios.post('http://localhost:8081/showTasks', values)
-        .then(res => {
-            if (res.data === "No tasks") {
-                // SHOW h1
-            } else if (res.data === "Error!") {
-                alert ("Something went wrong with showing the tasks.")
-            } else {
-                console.log(res.data)
-            }
-        })
-        .catch(err => console.log(err))
-    }, [])
+            .then(res => {
+                if (res.data === "No tasks") {
+                    setTasks([noTasksHeader]);
+                } else if (res.data === "Error!") {
+                    alert("Something went wrong with showing the tasks.");
+                } else {
+                    // Przekształć obiekt i utrzymanie tablicy
+                    const flatObject = res.data.map(task => (
+                        Object.entries(task).map(([key, value]) => `${key}: ${value}`).join('\n')
+                    ));
+
+                    setTasks(flatObject);
+                }
+            })
+            .catch(err => console.log(err));
+    }, [login]);
 
     return (
         <div className='showTasksPanel'>
-            {/* <textarea spellCheck="false" value={`ADKLJKALJDS: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} />
-            <textarea spellCheck="false" value={`Name: ${name}\n\nDescription: ${description}\n\nDate: ${date}\n\nPriority: ${priority}`} /> */}
-
-            {/* <h1> No tasks available. </h1> */}
+            {tasks.length === 1 && tasks[0] === noTasksHeader ? (
+                <h1>{noTasksHeader}</h1>
+            ) : (
+                tasks.map((task, index) => (
+                    <textarea
+                        key={index}
+                        spellCheck="false"
+                        value={task}
+                        readOnly
+                    />
+                ))
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default ShowTasksPanel
+export default ShowTasksPanel;
